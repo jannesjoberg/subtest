c0515126298576dee82728b37c50d24228d4628b
Jan Sjöberg <jannesjoberg@hotmail.com>
2016-09-06 14:26:38 +0200

    Lite grejs

---
+++ SubTest/SubTest/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubTest
+{
+  public class SubClass
+  {
+    public static string SecretString()
+    {
+      return "Hello world!";
+    }
+  }
+}
